d9cd0c53defb7b8f427ad281e0e271ec00a9d0d1
LiamCharles2001 <61995123+LiamCharles2001@users.noreply.github.com>
2024-03-23 09:40:40 -0400

    Hero Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <Nav />
-        <main className="container">{children}</main>
+        <main>{children}</main>
       </body>
     </html>
   );
