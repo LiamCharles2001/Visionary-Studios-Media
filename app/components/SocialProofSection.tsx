b2e443e11b2e8c524dac8768e3b2a566734c37fe
Charles <Laimp1343@hotmail.com>
2024-03-27 10:48:39 -0400

    Updated Words

--- app/components/SocialProofSection.tsx
+++ app/components/SocialProofSection.tsx
@@ -6,15 +6,15 @@ const SocialProofSection = () => {
             <dl class="grid max-w-screen-md gap-8 mx-auto text-gray-900 sm:grid-cols-3 dark:text-white">
                 <div class="flex flex-col items-center justify-center">
                     <dt class="mb-2 text-3xl md:text-4xl font-extrabold">73M+</dt>
-                    <dd class="font-light text-gray-500 dark:text-gray-400">developers</dd>
+                    <dd class="font-light text-gray-500 dark:text-gray-400">views</dd>
                 </div>
                 <div class="flex flex-col items-center justify-center">
                     <dt class="mb-2 text-3xl md:text-4xl font-extrabold">1B+</dt>
-                    <dd class="font-light text-gray-500 dark:text-gray-400">contributors</dd>
+                    <dd class="font-light text-gray-500 dark:text-gray-400">contributors??</dd>
                 </div>
                 <div class="flex flex-col items-center justify-center">
                     <dt class="mb-2 text-3xl md:text-4xl font-extrabold">4M+</dt>
-                    <dd class="font-light text-gray-500 dark:text-gray-400">organizations</dd>
+                    <dd class="font-light text-gray-500 dark:text-gray-400">organizations??</dd>
                 </div>
             </dl>
         </div>
