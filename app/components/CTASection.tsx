872c86f785a25a82b058d6e5bdcbc8d2475401b0
LiamCharles2001 <61995123+LiamCharles2001@users.noreply.github.com>
2024-03-29 22:51:11 -0400

    Better Images

--- app/components/CTASection.tsx
+++ app/components/CTASection.tsx
@@ -1,7 +1,9 @@
+import Image from "next/image";
+
 const CTASection = () => {
   return (
     
-<section className="bg-white dark:bg-gray-900 bg-[url('/assets/timeline-bottom.jpg')]">
+<section className="relative">
     <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
         <div className="max-w-screen-md">
             <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Let&apos;s talk about how we can help your videos.</h2>
@@ -14,6 +16,7 @@ const CTASection = () => {
             </div>
         </div>
     </div>
+    <Image objectFit='cover' fill src="/assets/timeline-bottom.jpg" alt="Bonnie Avatar" className="-z-10"/>
 </section>
   );
 };
