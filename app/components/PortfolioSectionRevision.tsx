b22b303079e44188c8ff0b07c6bd804ad8c6b26a
LiamCharles2001 <61995123+LiamCharles2001@users.noreply.github.com>
2024-04-03 17:35:47 -0400

    Update PortfolioSectionRevision.tsx

--- app/components/PortfolioSectionRevision.tsx
+++ app/components/PortfolioSectionRevision.tsx
@@ -4,10 +4,13 @@ const PortfolioSectionRevision = () => {
       <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
         <div className="max-w-screen-md mb-8 lg:mb-16">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-          Welcome to Our Studio: Crafting Professional Video Content for YouTubers and TikTokers
+            Welcome to Our Studio: Crafting Professional Video Content for
+            YouTubers and TikTokers
           </h2>
           <p className="text-gray-500 sm:text-xl dark:text-gray-400">
-          At our studio, we're not just video editors; we're storytellers. Our team specializes in partnering with YouTubers and TikTokers to elevate their content to new heights.
+            At our studio, we're not just video editors; we're storytellers. Our
+            team specializes in partnering with YouTubers and TikTokers to
+            elevate their content to new heights.
           </p>
         </div>
         <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
@@ -19,29 +22,16 @@ const PortfolioSectionRevision = () => {
                 viewBox="0 0 16 16"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <path d="M2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm6 3.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132"/></svg>
-            </div>
-            <h3 className="mb-2 text-xl font-bold dark:text-white">Collaborative Creation</h3>
-            <p className="text-gray-500 dark:text-gray-400">
-            We believe in the power of collaboration. From the initial brainstorming sessions to the final edits, we work closely with our clients to understand their vision and bring it to life.
-            </p>
-          </div>
-          <div>
-            <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-              <svg
-                className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
-                fill="currentColor"
-                viewBox="0 0 16 16"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M.102 2.223A3.004 3.004 0 0 0 3.78 5.897l6.341 6.252A3.003 3.003 0 0 0 13 16a3 3 0 1 0-.851-5.878L5.897 3.781A3.004 3.004 0 0 0 2.223.1l2.141 2.142L4 4l-1.757.364zm13.37 9.019.528.026.287.445.445.287.026.529L15 13l-.242.471-.026.529-.445.287-.287.445-.529.026L13 15l-.471-.242-.529-.026-.287-.445-.445-.287-.026-.529L11 13l.242-.471.026-.529.445-.287.287-.445.529-.026L13 11z"/>
-</svg>
+                <path d="M2 0a2 2 0 0 0-2 2v8a2 2 0 0 0 2 2h2.5a1 1 0 0 1 .8.4l1.9 2.533a1 1 0 0 0 1.6 0l1.9-2.533a1 1 0 0 1 .8-.4H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm6 3.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132" />
+              </svg>
             </div>
             <h3 className="mb-2 text-xl font-bold dark:text-white">
-            Crafting the Narrative
+              Collaborative Creation
             </h3>
             <p className="text-gray-500 dark:text-gray-400">
-            With state-of-the-art editing tools and a keen eye for detail, we meticulously craft each video, ensuring that every frame contributes to a compelling narrative that keeps viewers engaged.
+              We believe in the power of collaboration. From the initial
+              brainstorming sessions to the final edits, we work closely with
+              our clients to understand their vision and bring it to life.
             </p>
           </div>
           <div>
@@ -52,13 +42,38 @@ const PortfolioSectionRevision = () => {
                 viewBox="0 0 16 16"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
-  <path fillRule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"/>
-</svg>
+                <path d="M.102 2.223A3.004 3.004 0 0 0 3.78 5.897l6.341 6.252A3.003 3.003 0 0 0 13 16a3 3 0 1 0-.851-5.878L5.897 3.781A3.004 3.004 0 0 0 2.223.1l2.141 2.142L4 4l-1.757.364zm13.37 9.019.528.026.287.445.445.287.026.529L15 13l-.242.471-.026.529-.445.287-.287.445-.529.026L13 15l-.471-.242-.529-.026-.287-.445-.445-.287-.026-.529L11 13l.242-.471.026-.529.445-.287.287-.445.529-.026L13 11z" />
+              </svg>
             </div>
-            <h3 className="mb-2 text-xl font-bold dark:text-white">Feedback Driven</h3>
+            <h3 className="mb-2 text-xl font-bold dark:text-white">
+              Crafting the Narrative
+            </h3>
             <p className="text-gray-500 dark:text-gray-400">
-            Our process is built on feedback. We welcome input from our clients every step of the way, refining our edits until they perfectly align with their vision and brand.
+              Fostering collaboration from concept to completion, ensuring your
+              visions come to life in every frame.
+            </p>
+          </div>
+          <div>
+            <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+              <svg
+                className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
+                fill="currentColor"
+                viewBox="0 0 16 16"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                <path
+                  fillRule="evenodd"
+                  d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5z"
+                />
+              </svg>
+            </div>
+            <h3 className="mb-2 text-xl font-bold dark:text-white">
+              Feedback Driven
+            </h3>
+            <p className="text-gray-500 dark:text-gray-400">
+              Edits based on client feedback, ensuring perfect alignment with
+              their vision and brand.
             </p>
           </div>
           <div>
@@ -76,9 +91,12 @@ const PortfolioSectionRevision = () => {
                 />
               </svg>
             </div>
-            <h3 className="mb-2 text-xl font-bold dark:text-white">Impactful Results</h3>
+            <h3 className="mb-2 text-xl font-bold dark:text-white">
+              Impactful Results
+            </h3>
             <p className="text-gray-500 dark:text-gray-400">
-            The success of our videos is measured not just in views, but in the impact they have on audiences. From heartfelt comments to shares across social media, we're proud to see our work resonate with viewers worldwide.
+              Driving meaningful engagement and resonance, our videos spark
+              impactful conversations and global shares.
             </p>
           </div>
           <div>
@@ -89,15 +107,16 @@ const PortfolioSectionRevision = () => {
                 viewBox="0 0 16 16"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <path d="M12.17 9.53c2.307-2.592 3.278-4.684 3.641-6.218.21-.887.214-1.58.16-2.065a3.6 3.6 0 0 0-.108-.563 2 2 0 0 0-.078-.23V.453c-.073-.164-.168-.234-.352-.295a2 2 0 0 0-.16-.045 4 4 0 0 0-.57-.093c-.49-.044-1.19-.03-2.08.188-1.536.374-3.618 1.343-6.161 3.604l-2.4.238h-.006a2.55 2.55 0 0 0-1.524.734L.15 7.17a.512.512 0 0 0 .433.868l1.896-.271c.28-.04.592.013.955.132.232.076.437.16.655.248l.203.083c.196.816.66 1.58 1.275 2.195.613.614 1.376 1.08 2.191 1.277l.082.202c.089.218.173.424.249.657.118.363.172.676.132.956l-.271 1.9a.512.512 0 0 0 .867.433l2.382-2.386c.41-.41.668-.949.732-1.526zm.11-3.699c-.797.8-1.93.961-2.528.362-.598-.6-.436-1.733.361-2.532.798-.799 1.93-.96 2.528-.361s.437 1.732-.36 2.531Z"/>
-  <path d="M5.205 10.787a7.6 7.6 0 0 0 1.804 1.352c-1.118 1.007-4.929 2.028-5.054 1.903-.126-.127.737-4.189 1.839-5.18.346.69.837 1.35 1.411 1.925"/>
-</svg>
+                <path d="M12.17 9.53c2.307-2.592 3.278-4.684 3.641-6.218.21-.887.214-1.58.16-2.065a3.6 3.6 0 0 0-.108-.563 2 2 0 0 0-.078-.23V.453c-.073-.164-.168-.234-.352-.295a2 2 0 0 0-.16-.045 4 4 0 0 0-.57-.093c-.49-.044-1.19-.03-2.08.188-1.536.374-3.618 1.343-6.161 3.604l-2.4.238h-.006a2.55 2.55 0 0 0-1.524.734L.15 7.17a.512.512 0 0 0 .433.868l1.896-.271c.28-.04.592.013.955.132.232.076.437.16.655.248l.203.083c.196.816.66 1.58 1.275 2.195.613.614 1.376 1.08 2.191 1.277l.082.202c.089.218.173.424.249.657.118.363.172.676.132.956l-.271 1.9a.512.512 0 0 0 .867.433l2.382-2.386c.41-.41.668-.949.732-1.526zm.11-3.699c-.797.8-1.93.961-2.528.362-.598-.6-.436-1.733.361-2.532.798-.799 1.93-.96 2.528-.361s.437 1.732-.36 2.531Z" />
+                <path d="M5.205 10.787a7.6 7.6 0 0 0 1.804 1.352c-1.118 1.007-4.929 2.028-5.054 1.903-.126-.127.737-4.189 1.839-5.18.346.69.837 1.35 1.411 1.925" />
+              </svg>
             </div>
             <h3 className="mb-2 text-xl font-bold dark:text-white">
-            Staying Ahead
+              Staying Ahead
             </h3>
             <p className="text-gray-500 dark:text-gray-400">
-            In a rapidly evolving industry, we're committed to staying ahead of the curve. We're constantly exploring new techniques and technologies to push the boundaries of what's possible and deliver innovative content.
+              Pushing boundaries and innovating to stay ahead, ensuring our
+              content remains at the forefront of industry trends.
             </p>
           </div>
           <div>
@@ -108,14 +127,19 @@ const PortfolioSectionRevision = () => {
                 viewBox="0 0 16 16"
                 xmlns="http://www.w3.org/2000/svg"
               >
-                <path fillRule="evenodd" d="M11.5 1.246c.832-.855 2.913.642 0 2.566-2.913-1.924-.832-3.421 0-2.566M9 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0m-9 8c0 1 1 1 1 1h10s1 0 1-1-1-4-6-4-6 3-6 4m13.5-8.09c1.387-1.425 4.855 1.07 0 4.277-4.854-3.207-1.387-5.702 0-4.276ZM15 2.165c.555-.57 1.942.428 0 1.711-1.942-1.283-.555-2.281 0-1.71Z"/>
- </svg>
+                <path
+                  fillRule="evenodd"
+                  d="M11.5 1.246c.832-.855 2.913.642 0 2.566-2.913-1.924-.832-3.421 0-2.566M9 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0m-9 8c0 1 1 1 1 1h10s1 0 1-1-1-4-6-4-6 3-6 4m13.5-8.09c1.387-1.425 4.855 1.07 0 4.277-4.854-3.207-1.387-5.702 0-4.276ZM15 2.165c.555-.57 1.942.428 0 1.711-1.942-1.283-.555-2.281 0-1.71Z"
+                />
+              </svg>
             </div>
             <h3 className="mb-2 text-xl font-bold dark:text-white">
-            Client-Centric
+              Client-Centric
             </h3>
             <p className="text-gray-500 dark:text-gray-400">
-            Our client's satisfaction is our top priority. We prioritize open communication, transparency, and flexibility, ensuring a seamless and enjoyable experience throughout the entire video creation process.</p>
+              Centering on your satisfaction, we prioritize open communication
+              and flexibility throughout the creative process.
+            </p>
           </div>
         </div>
       </div>
