f001c6ba8b208ec0cad1c1e6badd451657e1f0e8
LiamCharles2001 <61995123+LiamCharles2001@users.noreply.github.com>
2024-04-11 23:54:14 -0400

    WIP hero Plugs

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -9,12 +9,15 @@ export default function Hero() {
         alt="Video Editing Service Logo"
         className="-z-20 brightness-75 object-cover"
         priority
-        placeholder = 'empty'
+        placeholder="empty"
       />
       <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7">
           <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl text-zinc-100">
-            <div className="underline decoration-primary-400">Video Editing </div>for Content Creators
+            <div className="underline decoration-primary-400">
+              Video Editing{" "}
+            </div>
+            for Content Creators
           </h1>
           <p className="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl text-zinc-200">
             Get your videos Professionally Edited in Your Personal Style
@@ -48,7 +51,74 @@ export default function Hero() {
           ></iframe>
         </div>
       </div>
-      
+
+      <div className="py-8 lg:py-16 mx-auto max-w-screen-xl px-4">
+        <h2 className="mb-8 lg:mb-16 text-3xl font-extrabold tracking-tight leading-tight text-center text-gray-900 dark:text-white md:text-4xl">
+          You’ll be in good company
+        </h2>
+        <div className="grid grid-cols-2 gap-8 text-gray-500 sm:gap-12 md:grid-cols-3 lg:grid-cols-6 dark:text-gray-400">
+          <a href="#" className="flex justify-center items-center">
+            <svg
+              className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-white"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z" />
+            </svg>
+          </a>
+          <a href="#" className="flex justify-center items-center">
+            <svg
+              className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-white"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z" />
+            </svg>
+          </a>
+          <a href="#" className="flex justify-center items-center">
+            <svg
+              className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-white"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z" />
+            </svg>
+          </a>
+          <a href="#" className="flex justify-center items-center">
+            <svg
+              className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-white"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z" />
+            </svg>
+          </a>
+          <a href="#" className="flex justify-center items-center">
+            <svg
+              className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-white"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z" />
+            </svg>
+          </a>
+          <a href="#" className="flex justify-center items-center">
+            <svg
+              className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-white"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z" />
+            </svg>
+          </a>
+        </div>
+      </div>
     </section>
   );
 }
