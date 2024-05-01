a234c7cca22840744c80f2cd9a98d9509872c151
LiamCharles2001 <61995123+LiamCharles2001@users.noreply.github.com>
2024-05-01 17:26:49 -0400

    Update Socials.tsx

--- app/components/Socials.tsx
+++ app/components/Socials.tsx
@@ -86,7 +86,7 @@ const Socials = () => {
           </div>
         </div>
 
-        <div className="grid gap-8 mb-6 lg:mb-16 md:grid-cols-2">
+        <div className="grid gap-8 mb-6 lg:mb-16 justify-center items-center">
           <div className="items-center rounded-lg shadow sm:flex bg-zinc-800 border-zinc-800">
             <div className="p-5">
               <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-500">
@@ -106,30 +106,8 @@ const Socials = () => {
               </h3>
 
               <p className="mb-4 font-light text-zinc-400">
-                contact@visionarystudios.ca
+              visionarystudiosmedia@gmail.com
               </p>
-            </div>
-          </div>
-          <div className="items-center rounded-lg shadow sm:flex bg-zinc-800 border-zinc-800">
-            <div className="p-5">
-              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-500">
-                <svg
-                  className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-white"
-                  fill="currentColor"
-                  viewBox="0 0 16 16"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"
-                  />{" "}
-                </svg>
-              </div>
-
-              <h3 className="text-xl font-bold tracking-tight text-white">
-                Phone
-              </h3>
-              <p className="mb-4 font-light text-zinc-400">+1 (905) 931-1220</p>
             </div>
           </div>
         </div>
